8cc4954ee98829e11f7c3d35bad3bb558ee877f1
xmfenbei <xmfenbei@outlook.com>
2018-06-27 09:48:47 +0800

    新建项目

---
+++ src/main/java/com/xmfenbei/dijkstra/DijkstraApplication.java
@@ -0,0 +1,12 @@
+package com.xmfenbei.dijkstra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DijkstraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DijkstraApplication.class, args);
+	}
+}
